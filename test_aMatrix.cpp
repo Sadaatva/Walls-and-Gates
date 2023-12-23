cbe5a5101491ece24fa82b7832685ac82da7af91
G0oCe <alim.sadigov20@gmail.com>
2023-12-23 22:45:31 +0400

    fix EmptyLabyrinthTest  and separete TwoEmptyCagesTest

--- test_aMatrix.cpp
+++ test_aMatrix.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include <vector>
+#include "buildAdjacencyMatrix.cpp"
 
-TEST(FirstTest, Empty) {
-	std::vector<std::vector<int>> labyrinth
-    ASSERT_EQ(0, buildAdjacencyMatrix(labyrinth));
+TEST(EmptyLabyrinthTest, Empty) {
+    std::vector<std::vector<int>> labyrinth = { {} };
+    ASSERT_EQ(0, buildAdjacencyMatrix(labyrinth).size());
 }
 
-TEST(SecondTest, oneEmptyCage) {
-	std::vector<std::vector<int>> labyrinth = {
+TEST(OneEmptyCageTest, OneEmptyCage) {
+    std::vector<std::vector<int>> labyrinth = {
         {2147483647},
     };
 
@@ -19,36 +20,44 @@ TEST(SecondTest, oneEmptyCage) {
     ASSERT_EQ(out, buildAdjacencyMatrix(labyrinth));
 }
 
-TEST(ThirdTest, twoEmptyCage) {
-	std::vector<std::vector<int>> labyrinth = {
+TEST(TwoEmptyCagesNearTest, TwoEmptyCagesNear) {
+    std::vector<std::vector<int>> labyrinth = {
         {2147483647, 2147483647},
     };
 
-    std::vector<std::vector<int>> Near = { //When cages is near
+    std::vector<std::vector<int>> near = {
         {0, 1},
         {1, 0}
     };
-    std::vector<std::vector<int>> notNear = { //When cages is not near
+
+    ASSERT_EQ(near, buildAdjacencyMatrix(labyrinth));
+}
+
+TEST(TwoEmptyCagesNotNearTest, TwoEmptyCagesNotNear) {
+    std::vector<std::vector<int>> labyrinth = {
+        {2147483647, -1},
+        {-1, 2147483647}
+    };
+
+     std::vector<std::vector<int>> notNear = {
         {0, 0, 0, 0},
 		{0, 0, 0, 0},
 		{0, 0, 0, 0},
 		{0, 0, 0, 0},
     };
-
-    ASSERT_EQ(Near, buildAdjacencyMatrix(labyrinth)); //When cages is near
-    ASSERT_EQ(notNear, buildAdjacencyMatrix(labyrinth)); //When cages is near
+    ASSERT_EQ(notNear, buildAdjacencyMatrix(labyrinth));
 }
 
-TEST(FourthTest, sample) {
-	std::vector<std::vector<int>> labyrinth = {
-		{2147483647, -1 ,0, 2147483647},
-		{2147483647,2147483647,2147483647,-1},
-		{2147483647,-1,2147483647,-1},
-		{0,-1,2147483647,2147483647}
-	};
+TEST(SampleTest, Sample) {
+    std::vector<std::vector<int>> labyrinth = {
+        {2147483647, -1, 0, 2147483647},
+        {2147483647, 2147483647, 2147483647, -1},
+        {2147483647, -1, 2147483647, -1},
+        {0, -1, 2147483647, 2147483647}
+    };
 
-	std::vector<std::vector<int>> out = {
-		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    std::vector<std::vector<int>> out = {
+        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -64,13 +73,12 @@ TEST(FourthTest, sample) {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-	};
+    };
 
     ASSERT_EQ(out, buildAdjacencyMatrix(labyrinth));
 }
 
 int main() {
-	
-	testing::InitGoogleTest();
+    testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
