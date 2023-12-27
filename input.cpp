8fa0367dee534409e8e7cc165668f593b1276985
G0oCe <alim.sadigov20@gmail.com>
2023-12-27 14:26:09 +0400

    add size and empty file check

--- input.cpp
+++ input.cpp
@@ -8,7 +8,12 @@ std::vector<std::vector<int>> readDataFromFile(const std::string& filename) {
 
     if (!file.is_open()) {
         std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return {};
+        return -1;
+    }
+
+    if (file.peek() == std::ifstream::traits_type::eof()) {
+        file.close();
+        return { { } };
     }
 
     std::vector<std::vector<int>> matrix;
@@ -20,13 +25,28 @@ std::vector<std::vector<int>> readDataFromFile(const std::string& filename) {
 
         int value;
         while (iss >> value) {
-              row.push_back(value);
+            row.push_back(value);
         }
-        
+
         matrix.push_back(row);
     }
 
     file.close();
+
+    // Check for an empty matrix
+    if (matrix.empty()) {
+        std::cerr << "Error: Empty matrix in file " << filename << std::endl;
+        return { };
+    }
+
+    // Check for matrix size exceeding 250x250
+    const size_t numRows = matrix.size();
+    const size_t numCols = row.size();
+
+    if (numRows > 250 || numCols > 250) {
+        std::cerr << "Error: Matrix size exceeds the limit of 250x250 in file " << filename << std::endl;
+        return -1;
+    }
+
     return matrix;
 }
-
