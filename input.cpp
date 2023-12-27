ad9f512308d5f0a224f1bb26211cf0471fff5f35
G0oCe <alim.sadigov20@gmail.com>
2023-12-27 15:01:27 +0400

    rewrite input function

--- input.cpp
+++ input.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <sstream>
 #include <fstream>
+#include <vector>
 
 std::vector<std::vector<int>> input(const std::string& filename) {
     std::ifstream file(filename);
 
     if (!file.is_open()) {
         std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return -1;
-    }
-
-    if (file.peek() == std::ifstream::traits_type::eof()) {
-        file.close();
-        return { { } };
+        return {};
     }
 
     std::vector<std::vector<int>> matrix;
@@ -32,21 +26,13 @@ std::vector<std::vector<int>> input(const std::string& filename) {
     }
 
     file.close();
-
-    // Check for an empty matrix
-    if (matrix.empty()) {
-        std::cerr << "Error: Empty matrix in file " << filename << std::endl;
-        return {  };
-    }
-
-    // Check for matrix size exceeding 250x250
-    const size_t numRows = matrix.size();
-    const size_t numCols = row.size();
-
-    if (numRows > 250 || numCols > 250) {
-        std::cerr << "Error: Matrix size exceeds the limit of 250x250 in file " << filename << std::endl;
-        return -1;
-    }
-
     return matrix;
+}
+
+int main() {
+    std::string filename = "example.txt"; // Укажите имя вашего файла
+
+    std::vector<std::vector<int>> myMatrix = readDataFromFile(filename);
+
+    return 0;
 }
